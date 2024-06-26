b48ce3e810bace3af289ddd6210b3115e1da91c2
AmanAsharma070320 <asharma070320@gmail.com>
2024-06-26 18:18:14 +0530

    Email&Password Validation With Formik&Yup

--- src/checking/index.js
+++ src/checking/index.js
@@ -3,8 +3,13 @@ import * as Yup from "yup";
 export const signUpSchema = Yup.object({
   name: Yup.string().min(2).max(25).required("Please enter your name"),
   email: Yup.string().email().required("Please enter your email"),
-  password: Yup.string().min(6).required("Please enter your password"),
-  confirm_password: Yup.string()
-    .required()
-    .oneOf([Yup.ref("password"), null], "Password must match"),
+  password: Yup.string()
+  .required('Password is required')
+  .matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+    'Password must contain at least one uppercase letter, one lowercase letter, and one number'
+  ),
+confirm_password: Yup.string()
+  .oneOf([Yup.ref('password'), null], 'Passwords must match')
+  .required('Confirm Password is required'),
 });
