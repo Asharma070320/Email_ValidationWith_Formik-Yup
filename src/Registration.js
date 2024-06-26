6e85c54f4ba92f33c456b210ee941f8b655450e7
AmanAsharma070320 <asharma070320@gmail.com>
2024-06-26 18:42:43 +0530

    Valid Email-Password With Formik & Yup

--- src/Registration.js
+++ src/Registration.js
@@ -40,108 +40,108 @@ const Registration = () => {
 
   return (
     <>
-      <GlobalStyle />
-      <Wrapper>
-        <div className="container">
-          <div className="modal">
-            <div className="modal-container">
-              <div className="modal-left">
-                <h1 className="modal-title">Welcome!</h1>
-                <form onSubmit={handleSubmit}>
-                  <div className="input-block">
-                    <label htmlFor="name" className="input-label">
-                      Name
-                    </label>
-                    <input
-                      type="name"
-                      autoComplete="off"
-                      name="name"
-                      id="name"
-                      placeholder="Name"
-                      value={values.name}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    {errors.name && touched.name ? (
-                      <p className="form-error">{errors.name}</p>
-                    ) : null}
-                  </div>
-                  <div className="input-block">
-                    <label htmlFor="email" className="input-label">
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      autoComplete="off"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                      value={values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    {errors.email && touched.email ? (
-                      <p className="form-error">{errors.email}</p>
-                    ) : null}
-                  </div>
-                  <div className="input-block">
-                    <label htmlFor="password" className="input-label">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      autoComplete="off"
-                      name="password"
-                      id="password"
-                      placeholder="Password"
-                      value={values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    {errors.password && touched.password ? (
-                      <p className="form-error">{errors.password}</p>
-                    ) : null}
-                  </div>
-                  <div className="input-block">
-                    <label htmlFor="confirm_password" className="input-label">
-                      Confirm Password
-                    </label>
-                    <input
-                      type="password"
-                      autoComplete="off"
-                      name="confirm_password"
-                      id="confirm_password"
-                      placeholder="Confirm Password"
-                      value={values.confirm_password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    {errors.confirm_password && touched.confirm_password ? (
-                      <p className="form-error">{errors.confirm_password}</p>
-                    ) : null}
-                  </div>
-                  <div className="modal-buttons">
-                    <button
-                      className="input-button"
-                      type="submit"
-                      disabled={!allFieldsFilled()}
-                    >
-                      Sign Up
-                    </button>
-                  </div>
-                </form>
-              </div>
-              <div className="modal-right">
-                <img
-                  src="https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-                  alt=""
-                />
-              </div>
+    <GlobalStyle />
+    <Wrapper>
+      <div className="container">
+        <div className="modal">
+          <div className="modal-container">
+            <div className="modal-left">
+              <h1 className="modal-title">Welcome!</h1>
+              <form onSubmit={handleSubmit}>
+                <div className="input-block">
+                  <label htmlFor="name" className="input-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    autoComplete="off"
+                    name="name"
+                    id="name"
+                    placeholder="Name"
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.name && touched.name ? (
+                    <p className="form-error">{errors.name}</p>
+                  ) : null}
+                </div>
+                <div className="input-block">
+                  <label htmlFor="email" className="input-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    autoComplete="off"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.email && touched.email ? (
+                    <p className="form-error">{errors.email}</p>
+                  ) : null}
+                </div>
+                <div className="input-block">
+                  <label htmlFor="password" className="input-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    autoComplete="off"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.password && touched.password ? (
+                    <p className="form-error">{errors.password}</p>
+                  ) : null}
+                </div>
+                <div className="input-block">
+                  <label htmlFor="confirm_password" className="input-label">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    autoComplete="off"
+                    name="confirm_password"
+                    id="confirm_password"
+                    placeholder="Confirm Password"
+                    value={values.confirm_password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.confirm_password && touched.confirm_password ? (
+                    <p className="form-error">{errors.confirm_password}</p>
+                  ) : null}
+                </div>
+                <div className="modal-buttons">
+                  <button
+                    className="input-button"
+                    type="submit"
+                    disabled={!allFieldsFilled()}
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="modal-right">
+              <img
+                src="https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
             </div>
           </div>
         </div>
-      </Wrapper>
-    </>
+      </div>
+    </Wrapper>
+  </>
   );
 };
 
--- src/checking/index.js
+++ src/checking/index.js
@@ -2,14 +2,35 @@ import * as Yup from "yup";
 
 export const signUpSchema = Yup.object({
   name: Yup.string().min(2).max(25).required("Please enter your name"),
-  email: Yup.string().email().required("Please enter your email"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .matches(/@gmail\.com$/, "Email must end with @gmail.com")
+    .matches(/regex@gmail\.com$/, "Email must be regex@gmail.com")
+    .required("Email is required"),
   password: Yup.string()
-  .required('Password is required')
   .matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
-    'Password must contain at least one uppercase letter, one lowercase letter, and one number'
-  ),
+    /rishiMe@199$/,
+    "Password must be start from r__M_9"
+  )
+  .required("Password is required"),
 confirm_password: Yup.string()
-  .oneOf([Yup.ref('password'), null], 'Passwords must match')
-  .required('Confirm Password is required'),
+  .oneOf([Yup.ref("password"), null], "Passwords must match")
+  .required("Confirm password is required"),
 });
+
+
+// import * as Yup from "yup";
+
+// export const signUpSchema = Yup.object({
+//   name: Yup.string().min(2).max(25).required("Please enter your name"),
+//   email: Yup.string().email().required("Please enter your email"),
+//   password: Yup.string()
+//   .matches(
+//     /rishiMe@199$/,
+//     "Password must be rishiMe@199"
+//   )
+//   .required("Password is required"),
+// confirm_password: Yup.string()
+//   .oneOf([Yup.ref("password"), null], "Passwords must match")
+//   .required("Confirm password is required"),
+// });
